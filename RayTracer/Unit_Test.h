03b118d939c79888d4ccc79e6647abcb1bc12d09
XUAN YANG <gameryangxuan@gmail.com>
2019-01-15 12:53:41 +0800

    add unit test class
    - unit test for vector class

---
+++ RayTracer/Unit_Test.h
@@ -0,0 +1,13 @@
+#pragma once
+class Unit_Test
+{
+public:
+
+	Unit_Test();
+
+	~Unit_Test();
+
+	void Test_Vector();
+
+};
+
--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -8,27 +8,3 @@ int main()
 	delete render;
 	return 0;
 }
-
-/*
-
-void Unit_Test_Vector()
-{
-	std::string Value = "Value";
-	std::string Normalize = "Normalize";
-
-	auto Display_Vector = [](Vector3 vec, std::string des = "Value") {std::cout << "The " << des << " of vector is x = " << vec.X() << " y = " << vec.Y() << " z = " << vec.Z() << std::endl; };
-	auto Display_Vector_Dot = [](Vector3 vec1, Vector3 vec2) {std::cout << "The dot value of them is " << vec1.Dot(vec2) << std::endl; };
-	auto Display_Vector_Length = [](Vector3 vec) {std::cout << "The length value  is " << vec.Lengh() << std::endl; };
-
-	Vector3 Vec1(3, 3, 3);
-	Vector3 Vec2(4, 2, 5);
-	Vector3 Vec3(1, 0, 0);
-	Vector3 Vec4(0.2, 0.1, 0.9);
-	Display_Vector(1 - Vec4);
-	Display_Vector(Vec1 + Vec2);
-	Display_Vector(Vec1 - Vec2);
-	Display_Vector(Vec1 * 3);
-	Display_Vector_Length(Vec1);
-	Display_Vector_Dot(Vec1, Vec2);
-	Display_Vector(Vec1.Normalize(), Normalize);
-}*/
